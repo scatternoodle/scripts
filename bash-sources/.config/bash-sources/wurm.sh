77c4e4a64b2c4f91100212e959594af8eab1a58a
Elliot Shirra (Scatternoodle) <153317476+scatternoodle@users.noreply.github.com>
2025-12-21 23:09:16 +0000

    add wurm unlimited specific variabled

---
+++ bash-sources/.config/bash-sources/wurm.sh
@@ -0,0 +1,3 @@
+export WURMU_CLIENT="$HOME/.local/share/Steam/steamapps/common/Wurm Unlimited/WurmLauncher"
+export WURMU_SERVER="$HOME/.local/share/Steam/steamapps/common/Wurm Unlimited/WurmServerLauncher"
+export WURMU_DEDICATED="$HOME/.local/share/Steam/steamapps/common/Wurm Unlimited Dedicated Server"
